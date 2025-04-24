46d27b9a88e295fde88b2175d7e0e7faa857c29b
Albert-Gavra <albert.gavra@gmail.com>
2025-04-24 13:32:01 +0700

    Tugas Run Test

---
+++ cypress/e2e/search.form.js
@@ -0,0 +1,14 @@
+const SEARCHFORM = '#searchTerm';
+const SEARCHRESULT = 'The following pages were found for the query: ';
+
+class SearchForm {
+    static fillSearch (input) {
+        cy.get(SEARCHFORM).type(input);
+    }
+
+    static checkSearch (result) {
+        cy.contains(SEARCHRESULT).should('include.text', result);
+    }
+}
+
+export default SearchForm;
--- cypress/e2e/search.js
+++ cypress/e2e/search.js
@@ -1,17 +1,19 @@
+import SearchForm from "./search.form";
+import LoginPage from "./login.page";
+
 const { Given, When, Then} = require('@badeball/cypress-cucumber-preprocessor')
 
 Given('I login and open homepage', () => {
-    cy.visit('http://zero.webappsecurity.com/login.html');
-    cy.get('#user_login').type('username');
-    cy.get('#user_password').type('password');
-    cy.contains('Sign in').click();
-    cy.get('#account_summary_tab > a').should('be.visible');
+    LoginPage.visit();
+    LoginPage.fillUsername('username');
+    LoginPage.fillPassword('password');
+    LoginPage.signIn();
 })
 
 When('I input word "Bank" in search form', () => {
-    cy.get('#searchTerm').type('Bank{enter}');
+    SearchForm.fillSearch('Bank{enter}');
 })
 
 Then('I see the result of word "Bank"', () => {
-    cy.contains('The following pages were found for the query: ').should('include.text', 'bank');
+    SearchForm.checkSearch('Bank')
 })
